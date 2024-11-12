8944a76ab487f71569245d536c1621d010e3d69b
Nguyen Pham <baonguyenyam@gmail.com>
2024-11-12 12:26:18 -0600

    sadsad

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,10 @@ export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
+      <p className="text-default-600 mt-2">
+        My name is Nguyen Pham, I am a software engineer and I love to build
+        things.
+      </p>
     </div>
   );
 }
--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -21,6 +21,7 @@ export default function Home() {
   }
   const submitFormKeyword = () => {
     const keyword = document.getElementById('keyword') as HTMLInputElement;
+    const keywordsArray = keyword.value.split(',');
     if (!keyword.value) {
       messageApi.open({
         type: 'error',
@@ -28,7 +29,18 @@ export default function Home() {
       });
       return;
     }
-    setKeywords([...keywords, keyword.value]);
+    // Unique
+    keywordsArray.forEach((kw) => {
+      if (keywords.includes(kw)) {
+        messageApi.open({
+          type: 'error',
+          content: 'This keyword already exists',
+        });
+        return;
+      } else {
+        setKeywords([...keywords, ...keywordsArray]);
+      }
+    });
     keyword.value = '';
     setIsKeywordOpen(false);
   }
@@ -49,6 +61,7 @@ export default function Home() {
   }
   const submitFormSubKeyword = () => {
     const subKeyword = document.getElementById('subKeyword') as HTMLInputElement;
+    const subKeywordsArray = subKeyword.value.split(',');
     if (!subKeyword.value) {
       messageApi.open({
         type: 'error',
@@ -56,7 +69,18 @@ export default function Home() {
       });
       return;
     }
-    setSubKeywords([...subKeywords, subKeyword.value]);
+    // Unique
+    subKeywordsArray.forEach((kw) => {
+      if (subKeywords.includes(kw)) {
+        messageApi.open({
+          type: 'error',
+          content: 'This sub keyword already exists',
+        });
+        return;
+      } else {
+        setSubKeywords([...subKeywords, ...subKeywordsArray]);
+      }
+    });
     subKeyword.value = '';
     setIsSubKeywordOpen(false);
   }
@@ -77,6 +101,7 @@ export default function Home() {
   }
   const submitFormState = () => {
     const state = document.getElementById('state') as HTMLInputElement;
+    const stateArray = state.value.split(',');
     if (!state.value) {
       messageApi.open({
         type: 'error',
@@ -84,7 +109,18 @@ export default function Home() {
       });
       return;
     }
-    setStates([...states, state.value]);
+    // Unique
+    stateArray.forEach((st) => {
+      if (states.includes(st)) {
+        messageApi.open({
+          type: 'error',
+          content: 'This state already exists',
+        });
+        return;
+      } else {
+        setStates([...states, ...stateArray]);
+      }
+    });
     state.value = '';
     setIsStateOpen(false);
   }
@@ -105,6 +141,7 @@ export default function Home() {
   }
   const submitFormCity = () => {
     const city = document.getElementById('city') as HTMLInputElement;
+    const cityArray = city.value.split(',');
     if (!city.value) {
       messageApi.open({
         type: 'error',
@@ -112,7 +149,18 @@ export default function Home() {
       });
       return;
     }
-    setCities([...cities, city.value]);
+    // Unique
+    cityArray.forEach((ct) => {
+      if (cities.includes(ct)) {
+        messageApi.open({
+          type: 'error',
+          content: 'This city already exists',
+        });
+        return;
+      } else {
+        setCities([...cities, ...cityArray]);
+      }
+    });
     city.value = '';
     setIsCityOpen(false);
   }
@@ -133,6 +181,7 @@ export default function Home() {
   }
   const submitFormAddonOne = () => {
     const addonOneInput = document.getElementById('addonOne') as HTMLInputElement;
+    const addonOneArray = addonOneInput.value.split(',');
     if (!addonOneInput.value) {
       messageApi.open({
         type: 'error',
@@ -140,7 +189,18 @@ export default function Home() {
       });
       return;
     }
-    setAddonOne([...addonOne, addonOneInput.value]);
+    // Unique
+    addonOneArray.forEach((ad1) => {
+      if (addonOne.includes(ad1)) {
+        messageApi.open({
+          type: 'error',
+          content: 'This addonOne already exists',
+        });
+        return;
+      } else {
+        setAddonOne([...addonOne, ...addonOneArray]);
+      }
+    });
     addonOneInput.value = '';
     setIsAddonOneOpen(false);
   }
@@ -161,6 +221,7 @@ export default function Home() {
   }
   const submitFormAddonTwo = () => {
       const addonTwoInput = document.getElementById('addonTwo') as HTMLInputElement;
+      const addonTwoArray = addonTwoInput.value.split(',');
       if (!addonTwoInput.value) {
         messageApi.open({
           type: 'error',
@@ -168,7 +229,18 @@ export default function Home() {
         });
         return;
       }
-      setAddonTwo([...addonTwo, addonTwoInput.value]);
+      // Unique
+      addonTwoArray.forEach((ad2) => {
+        if (addonTwo.includes(ad2)) {
+          messageApi.open({
+            type: 'error',
+            content: 'This addonTwo already exists',
+          });
+          return;
+        } else {
+          setAddonTwo([...addonTwo, ...addonTwoArray]);
+        }
+      })
       addonTwoInput.value = '';
       setIsAddonTwoOpen(false);
     }
@@ -179,20 +251,150 @@ export default function Home() {
   //////////////////////////////////////////
   const [organaization, setOrganization] = useState([
       'keyword',
-      'subkeyword',
+      'subKeywords',
       'state',
-      'city',
-      'addonone',
-      'addontwo'
+      'cities',
+      'addonOne',
+      'addonTwo'
     ]
   );
+
+  const returnCombinations = (a: any[], b: any[], c: any[], d: any[], e: any[], f: any[]): any[] => {
+    // keywords = [A, B, C];
+    // subKeywords = [D, E, F];
+    // states = [G, H, I];
+    // cities = [J, K, L];
+    // addonOne = [M, N, O];
+    // addonTwo = [P, Q, R];
+    // // Generate all possible combinations
+    // - A D G J M P
+    // - A D G J M Q
+    // - ...
+    let list = [];
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+        list.push(`${a[i]} ${b[j]}`);
+        for (let k = 0; k < c.length; k++) {
+          list.push(`${a[i]} ${b[j]} ${c[k]}`);
+          for (let l = 0; l < d.length; l++) {
+            list.push(`${a[i]} ${b[j]} ${c[k]} ${d[l]}`);
+            for (let m = 0; m < e.length; m++) {
+              list.push(`${a[i]} ${b[j]} ${c[k]} ${d[l]} ${e[m]}`);
+              for (let n = 0; n < f.length; n++) {
+                list.push(`${a[i]} ${b[j]} ${c[k]} ${d[l]} ${e[m]} ${f[n]}`);
+              }
+            }
+          }
+        }
+      }
+    }
+    return list;
+  }
+
+  // generateKeywords
+  const [generatedKeywords, setGeneratedKeywords] = useState<any[]>([]);
+  const generateKeywords = () => {
+    let get_keywords = []
+    let get_subKeywords = []
+    let get_states = []
+    let get_cities = []
+    let get_addonOne = []
+    let get_addonTwo = []
+    if(organaization[0].toString() === 'keyword') {
+      get_keywords = keywords;
+    } else if(organaization[0].toString() === 'subKeywords') {
+      get_keywords = subKeywords;
+    } else if(organaization[0].toString() === 'state') {
+      get_keywords = states;
+    } else if(organaization[0].toString() === 'cities') {
+      get_keywords = cities;
+    } else if(organaization[0].toString() === 'addonOne') {
+      get_keywords = addonOne;
+    } else if(organaization[0].toString() === 'addonTwo') {
+      get_keywords = addonTwo;
+    }
+    // Sub Keywords
+    if(organaization[1].toString() === 'keyword') {
+      get_subKeywords = keywords;
+    } else if(organaization[1].toString() === 'subKeywords') {
+      get_subKeywords = subKeywords;
+    } else if(organaization[1].toString() === 'state') {
+      get_subKeywords = states;
+    } else if(organaization[1].toString() === 'cities') {
+      get_subKeywords = cities;
+    } else if(organaization[1].toString() === 'addonOne') {
+      get_subKeywords = addonOne;
+    } else if(organaization[1].toString() === 'addonTwo') {
+      get_subKeywords = addonTwo;
+    }
+    // States
+    if(organaization[2].toString() === 'keyword') {
+      get_states = keywords;
+    } else if(organaization[2].toString() === 'subKeywords') {
+      get_states = subKeywords;
+    } else if(organaization[2].toString() === 'state') {
+      get_states = states;
+    } else if(organaization[2].toString() === 'cities') {
+      get_states = cities;
+    } else if(organaization[2].toString() === 'addonOne') {
+      get_states = addonOne;
+    } else if(organaization[2].toString() === 'addonTwo') {
+      get_states = addonTwo;
+    }
+    // Cities
+    if(organaization[3].toString() === 'keyword') {
+      get_cities = keywords;
+    } else if(organaization[3].toString() === 'subKeywords') {
+      get_cities = subKeywords;
+    } else if(organaization[3].toString() === 'state') {
+      get_cities = states;
+    } else if(organaization[3].toString() === 'cities') {
+      get_cities = cities;
+    } else if(organaization[3].toString() === 'addonOne') {
+      get_cities = addonOne;
+    } else if(organaization[3].toString() === 'addonTwo') {
+      get_cities = addonTwo;
+    }
+    // AddonOne
+    if(organaization[4].toString() === 'keyword') {
+      get_addonOne = keywords;
+    } else if(organaization[4].toString() === 'subKeywords') {
+      get_addonOne = subKeywords;
+    } else if(organaization[4].toString() === 'state') {
+      get_addonOne = states;
+    } else if(organaization[4].toString() === 'cities') {
+      get_addonOne = cities;
+    } else if(organaization[4].toString() === 'addonOne') {
+      get_addonOne = addonOne;
+    } else if(organaization[4].toString() === 'addonTwo') {
+      get_addonOne = addonTwo;
+    }
+    // AddonTwo
+    if(organaization[5].toString() === 'keyword') {
+      get_addonTwo = keywords;
+    } else if(organaization[5].toString() === 'subKeywords') {
+      get_addonTwo = subKeywords;
+    } else if(organaization[5].toString() === 'state') {
+      get_addonTwo = states;
+    } else if(organaization[5].toString() === 'cities') {
+      get_addonTwo = cities;
+    } else if(organaization[5].toString() === 'addonOne') {
+      get_addonTwo = addonOne;
+    } else if(organaization[5].toString() === 'addonTwo') {
+      get_addonTwo = addonTwo;
+    }
+
+    let result = returnCombinations(get_keywords, get_subKeywords, get_states, get_cities, get_addonOne, get_addonTwo);
+    setGeneratedKeywords(result);
+  }
 
   return (
     <>
       {contextHolder}
       <hr className="my-0" />
+      <h2 className="text-3xl font-bold mt-3">Keywords</h2>
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">Keywords</h1>
+        <h3 className="text-lg font-bold">Keywords</h3>
         <div className="flex flex-wrap gap-2">
           {keywords.map((keyword) => (
             <span
@@ -229,7 +431,7 @@ export default function Home() {
       </section>
       <hr className="my-0" />
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">Sub Keywords</h1>
+        <h3 className="text-lg font-bold">Sub Keywords</h3>
         <div className="flex flex-wrap gap-2">
           {subKeywords.map((subKeyword) => (
             <span
@@ -266,7 +468,7 @@ export default function Home() {
       </section>
       <hr className="my-0" />
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">States</h1>
+        <h3 className="text-lg font-bold">States</h3>
         <div className="flex flex-wrap gap-2">
           {states.map((state) => (
             <span
@@ -303,7 +505,7 @@ export default function Home() {
       </section>
       <hr className="my-0" />
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">Cities</h1>
+        <h3 className="text-lg font-bold">Cities</h3>
         <div className="flex flex-wrap gap-2">
           {cities.map((city) => (
             <span
@@ -340,7 +542,7 @@ export default function Home() {
       </section>
       <hr className="my-0" />
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">Addons 1</h1>
+        <h3 className="text-lg font-bold">Addons 1</h3>
         <div className="flex flex-wrap gap-2">
           {addonOne.map((ad1) => (
             <span
@@ -377,7 +579,7 @@ export default function Home() {
       </section>
       <hr className="my-0" />
       <section className="flex flex-col gap-4 py-3 md:py-5">
-        <h1 className="text-2xl font-bold">Addons 2</h1>
+        <h3 className="text-lg font-bold">Addons 2</h3>
         <div className="flex flex-wrap gap-2">
           {addonTwo.map((ad2) => (
             <span
@@ -413,17 +615,39 @@ export default function Home() {
         </Modal>
       </section>
 
-      <hr className="my-5" />
+      <h2 className="text-3xl font-bold my-3">Organization</h2>
       <ReactSortable 
       list={organaization} 
       setList={setOrganization}>
         {organaization.map((item) => (
-          <div key={item} className="py-1 px-3 font-bold bg-gray-200 rounded-lg my-1 uppercase cursor-pointer">
+          <div key={item} className="py-1 px-3 font-bold bg-gray-200 rounded-lg my-1 uppercase cursor-pointer flex flex-row items-center">
             <MenuOutlined className="mr-2"/>
             {item}
           </div>
         ))}
       </ReactSortable>
+
+
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-5"
+      onClick={() => {generateKeywords()}}
+      >Generate</button>
+
+      <h2 className="text-3xl font-bold mt-10 mb-3">Generated Keywords {generatedKeywords.length? `(${generatedKeywords.length})`: ''}</h2>
+      <section className="flex flex-col gap-4 py-3 md:py-5">
+        <table className="w-full border border-gray-200">
+          <tbody>
+            {generatedKeywords.map((keyword) => (
+              <tr
+                key={keyword}
+                className="border-b border-gray-200"
+              >
+                <td className="px-3 py-1">{keyword}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+
     </>
   );
 }
